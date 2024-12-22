f44240d45115f94b28abf922e29e792a16f3c0d3
Himanshu Soni <39999787+blaringblazehs@users.noreply.github.com>
2024-12-22 10:08:07 +0530

    feat: login Register

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -1,10 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from "./auth.module.css"
 import loginImg from "../../assets/login.png"
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import Card from '../../components/card/Card'
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from '../../firebase/config'
+import { toast } from 'react-toastify'
+import { Loader } from '../../components'
+
 const Login = () => {
+    const [email,setEmail] = useState("");
+    const [password,setPassword] = useState("");
+    const [isLoading,setIsLoading] = useState(false);
+    const navigate = useNavigate();
+
+    const loginUser = (e)=>{
+        e.preventDefault();
+        setIsLoading(true);
+        signInWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+          // Signed in 
+          const user = userCredential.user;
+          toast.success("Login successful");
+          setIsLoading(false);
+          navigate("/")
+        })
+        .catch((error) => {
+          setIsLoading(false);
+          const errorCode = error.code;
+          const errorMessage = error.message;
+        });
+    }
+
   return (
+    <>
+    {isLoading && <Loader />}
     <div className={styles.container}>
       <Card>
       <div className={styles.loginContainer}>
@@ -14,9 +44,11 @@ const Login = () => {
         <div className={styles.rightContainer}>
             <h2>Login</h2>
             
-            <form>
-              <input type="text" placeholder='Email' required />
-              <input type="password" placeholder='Password' required />
+            <form onSubmit={loginUser}>
+              <input type="text" placeholder='Email' value={email} 
+                      onChange={(e)=> setEmail(e.target.value)} required />
+              <input type="password" placeholder='Password' value={password} 
+                        onChange={(e)=> setPassword(e.target.value)}  required />
               <button className={styles.btn}>Login</button>
               <div>
                 <Link to='/reset' >Reset Password</Link>
@@ -31,6 +63,7 @@ const Login = () => {
       </div>
       </Card>
     </div>
+    </>
   )
 }
 
--- src/pages/auth/Register.jsx
+++ src/pages/auth/Register.jsx
@@ -1,10 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from "./auth.module.css"
 import Card from "../../components/card/Card"
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import registerImg from "../../assets/register.png"
+import { ToastContainer, toast } from 'react-toastify';
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import {auth} from "../../firebase/config"
+import { Loader } from '../../components'
+
 const Register = () => {
+  const [email,setEmail] = useState("");
+  const [password,setPassword] = useState("");
+  const [cPassword,setCPassword] = useState("");
+  const [isLoading,setIsLoading] = useState(false);
+
+  const navigate  = useNavigate();
+
+  const registerUser = (e)=> {
+    e.preventDefault();
+    console.log(email,password,cPassword);
+    if(password !== cPassword){
+      toast.error("Password does not match")
+    }
+    setIsLoading(true);
+    const auth = getAuth();
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed up 
+        const user = userCredential.user;
+        // ...
+        setIsLoading(false);
+        toast.success("Registration successful")
+        navigate("/login")
+      })
+      .catch((error) => {
+        const errorMessage = error.message;
+        toast.error(errorMessage);
+        setIsLoading(false)
+      });
+  }
+
   return (
+    <>
+    <ToastContainer />
+    {isLoading && <Loader />}
     <div className={styles.container}>
       <Card>
       <div className={styles.loginContainer}>
@@ -12,11 +51,22 @@ const Register = () => {
         <div className={styles.rightContainer}>
             <h2>Login</h2>
             
-            <form>
-              <input type="text" placeholder='Email' required />
-              <input type="password" placeholder='Password' required />
-              <input type="password" placeholder='Confirm Password' required />
-              <button className={styles.btn}>Register</button>
+            <form onSubmit={registerUser}>
+              <input type="text" placeholder='Email' value={email} 
+                      onChange={(e)=> setEmail(e.target.value)}
+                      required />
+              <input type="password" 
+                        placeholder='Password' 
+                        value={password} 
+                        onChange={(e)=> setPassword(e.target.value)} 
+                        required 
+                      />
+              <input type="password"
+                        placeholder='Confirm Password'
+                        value={cPassword}
+                        onChange={(e)=> setCPassword(e.target.value)}
+                        required />
+              <button type='submit' className={styles.btn}>Register</button>
               {/* <div>
                 <Link to='/reset' >Reset Password</Link>
               </div> */}
@@ -33,6 +83,7 @@ const Register = () => {
       </div>
       </Card>
     </div>
+    </>
   )
 }
 
